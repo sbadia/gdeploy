96a741d89f529115ff7c6bffa77e14528fd20a46
Sebastien Badia <sebastien.badia@inria.fr>
2011-04-17 22:30:12 +0200

    [api] Add deploy with api

---
+++ deploy-api.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/ruby -w
+
+# ssh -NL 3443:api.grid5000.fr:443 lille.userl
+#
+require 'rubygems'
+require 'restfully' # gem install restfully
+
+USER = ENV['USER']
+
+logger = Logger.new(STDERR)
+logger.level = Logger::WARN
+Restfully::Session.new(:logger => logger, :base_uri => 'https://localhost:3443/sid/grid5000') do |root, session|
+  begin
+    job_to_submit = {
+      :resources => "nodes=10,walltime=01:00:00",
+      :command => "sleep 3600",
+      :properties => "cluster='graphene'",
+      :types => ["deploy"],
+      :name => 'demo-g5ks',
+      :project => 'gLite on Grid5000'
+    }
+    puts "*** Submitting the following job: #{job_to_submit.inspect}"
+    job = root.sites[:nancy].jobs.submit(job_to_submit)
+    while job.reload['state'] != 'running'
+      puts "Waiting for the job##{job['uid']} to be running..."
+      sleep 3
+    end
+    deployment_to_submit = {
+      :nodes => job['assigned_nodes'],
+      :environment => 'http://public.nancy.grid5000.fr/~sbadia/sl55-ahci.dsc',
+      :key => 'http://public.nancy.grid5000.fr/~sbadia/id_dsa.pub',
+      :notifications =>  ["xmpp:#{USER}@jabber.grid5000.fr"]
+    }
+    puts "OK"
+    puts "*** Launching the following deployment: #{deployment_to_submit.inspect}"
+    deployment = root.sites[:nancy].deployments.submit(deployment_to_submit)
+    while deployment.reload['status'] == 'processing'
+      puts "Waiting for the deployment##{deployment['uid']} to be terminated..."
+      sleep 30
+    end
+    puts "OK: #{deployment.inspect}"
+  rescue => e
+    puts "Error: #{e.class.name}, #{e.message}"
+  end
+end
