3b777f1c42d99fdb072724e39248d62643ba06de
Sebastien Badia <sebastien.badia@inria.fr>
2011-04-28 11:51:10 +0200

    [libs] Add restfully for tests

---
+++ net/restfully/lib/restfully.rb
@@ -0,0 +1,22 @@
+require 'backports'
+require 'yaml'
+require 'restfully/error'
+require 'restfully/parsing'
+require 'restfully/http'
+require 'restfully/http/adapters/rest_client_adapter'
+require 'restfully/extensions'
+require 'restfully/session'
+require 'restfully/special_hash'
+require 'restfully/special_array'
+require 'restfully/link'
+require 'restfully/resource'
+require 'restfully/collection'
+
+
+module Restfully
+  VERSION = "0.5.10"
+  class << self
+    attr_accessor :adapter
+  end
+  self.adapter = Restfully::HTTP::Adapters::RestClientAdapter
+end
