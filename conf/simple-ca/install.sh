8b190b3e9cf681a48e2defaf48ceeb52c8c97c36
Sebastien Badia <sebastien.badia@inria.fr>
2011-07-07 12:10:09 +0200

    Better script for install ca hash/cert

--- conf/simple-ca/install.sh
+++ conf/simple-ca/install.sh
@@ -4,6 +4,8 @@ set -e
 export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:$PATH"
 export GLOBUS_LOCATION="/opt/globus"
 export GPT_LOCATION="/opt/gpt"
-$GPT_LOCATION/sbin/gpt-build /root/ca.tgz gcc32dbg
+HASH=`cat /root/hash`
+cd /root
+$GPT_LOCATION/sbin/gpt-build globus_simple_ca_${HASH}_setup-0.18.tar.gz gcc32dbg
 $GPT_LOCATION/sbin/gpt-postinstall
-$GLOBUS_LOCATION/setup/globus_simple_ca_*_setup/setup-gsi -default
+$GLOBUS_LOCATION/setup/globus_simple_ca_${HASH}_setup/setup-gsi -default
