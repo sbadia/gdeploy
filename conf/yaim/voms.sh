2631f997640eefe80a3e53d85e0fbd06c42e001a
Sebastien Badia <sebastien.badia@inria.fr>
2011-07-14 10:26:35 +0200

    Add exports and tty for voms

--- conf/yaim/voms.sh
+++ conf/yaim/voms.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-TERM=xterm
+export TERM="xterm"
+export SHELL="/bin/bash"
+export SSH_TTY="/dev/pts/0"
 chmod -R 600 /root/yaim
 /opt/glite/yaim/bin/yaim -c -s /root/yaim/site-info.def -n VOMS
--- conf/yaim/wn.sh
+++ conf/yaim/wn.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-TERM=xterm
+export TERM="xterm"
+export SHELL="/bin/bash"
 chmod -R 600 /root/yaim
 /opt/glite/yaim/bin/yaim -c -s /root/yaim/site-info.def -n glite-WN -n TORQUE_client
