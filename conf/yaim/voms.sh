1a8ec6d420b75f49134a543e52605f84f2967237
Sebastien Badia <sebastien.badia@inria.fr>
2011-09-14 15:51:00 +0200

    Tests voms timeouts

--- conf/yaim/voms.sh
+++ conf/yaim/voms.sh
@@ -3,4 +3,7 @@ export TERM="xterm"
 export SHELL="/bin/bash"
 export SSH_TTY="/dev/pts/0"
 chmod -R 600 /root/yaim
-/opt/glite/yaim/bin/yaim -c -s /root/yaim/site-info.def -n VOMS
+/opt/glite/yaim/bin/yaim -c -s /root/yaim/site-info.def -n VOMS &
+#real	0m26.124s
+#user	0m11.881s
+#sys	0m2.195s
